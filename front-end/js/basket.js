af3af685399efa624f460d1b4b3a603cbfdef85f
maxime <mxderouet@gmail.com>
2021-01-19 10:10:20 +0100

    checkout logic

--- front-end/js/basket.js
+++ front-end/js/basket.js
@@ -4,78 +4,96 @@ getLocalStorage = () => {
 
 getLocalStorage();
 
-// envoyer requête POST à l'API avec {{ contact}, [ids_produits] }
-
 function serializeFormAndBasket() {
-    const apiMessage = {
+    let apiMessage = {
         contact: { 
-            firstName: document.getElementById().value,
-            lastName: document.getElementById().value,
-            address: document.getElementById().value,
-            city: document.getElementById().value,
-            email: document.getElementById().value
+            firstName: document.getElementById('firstname').value,
+            lastName: document.getElementById('lastname').value,
+            address: document.getElementById('address').value,
+            city: document.getElementById('city').value,
+            email: document.getElementById('email').value
         },
         products: []
     }
     const basket = JSON.parse(localStorage.getItem("cart"));
-    // boucle sur le basket pour alimenter le panier
-    for (item in basket) {
-        products.push(item._id);
+    let orderPrice = 0;
+    for (item of basket) {
+        console.log(item);
+        apiMessage.products.push(item._id);
+        orderPrice += (item.price * item.quantity);
     }
-    // apiMessage.products.push(id)
-    apiMessage = JSON.stringify(apiMessage);
-    return apiMessage;
+    return { apiMessage, orderPrice }
 }
 
 async function sendToApi(data) {
-    // sendToApi avec POST
-
-    const orderNumber = 'ApiResponse';
-    // envoyer numbero de commande & total dans URL pour page confirmation
-    // afficher numero de commande & total sur la page confirmation
-    // vider localStorage
-    return orderNumber;
+    console.log(data);
+    const request = new XMLHttpRequest();
+    request.open("POST", postUrl, true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.onreadystatechange = function () {
+        if (
+            this.readyState == XMLHttpRequest.DONE &&
+            this.status >= 200 &&
+            this.status < 400
+        ) {
+            const request = JSON.parse(this.responseText);
+            const orderNumber = request.orderId;
+            const orderPrice = data.orderPrice;
+            document.location.href = '/checkout.html#' + orderNumber + '&' + orderPrice;
+        }
+    };
+    request.send(JSON.stringify(data.apiMessage)); 
+    let basket = [];
+    basket = JSON.parse(localStorage.getItem("cart"));
+    basket.length = 0;
+    localStorage.setItem("cart", JSON.stringify(basket));
 }
 
-async function sendFormButton() {
-    const sendFormButton = createTag("input", "type", "submit");
-    sendFormButton.value = "Send form";
-    sendFormButton.addEventListener("click", () => {
+function sendFormButton() {
+    const formButton = createTag("input", "type", "submit");
+    formButton.value = "Send form";
+    formButton.addEventListener("click", async (e) => {
+        e.preventDefault();
         const data = serializeFormAndBasket();
         const orderNumber = await sendToApi(data);
         console.log(orderNumber);
     })
-    return sendFormButton;
+    return formButton;
 }
 
 function buildForm() {
     const form = document.getElementById("formCard");
     const formTag = createTag("form", "method", "get");
     const firstnameInput = createTag("input", "name", "firstname");
+    firstnameInput.setAttribute("id", "firstname");
     const labelFirstname = createTag("label", "for", "firstname");
     firstnameInput.setAttribute("pattern", "^[a-zA-Z ,.'-]+$");
     firstnameInput.required = true;
     labelFirstname.textContent = "Firstname:";
 
     const lastnameInput = createTag("input", "name", "lastname");
+    lastnameInput.setAttribute("id", "lastname");
     const labelLastname = createTag("label", "for", "lastname");
     lastnameInput.setAttribute("pattern", "^[a-zA-Z ,.'-]+$");
     lastnameInput.required = true;
     labelLastname.textContent = "Lastname:";
 
     const addressInput = createTag("input", "name", "address");
+    addressInput.setAttribute("id", "address");
     const labelAddress = createTag("label", "for", "address");
-    addressInput.setAttribute("pattern", "^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$");
+    addressInput.setAttribute("pattern", "^[a-zA-Z0-9 ,.'-]+$");
     addressInput.required = true;
     labelAddress.textContent = "Address:";
 
     const cityInput = createTag("input", "name", "city");
+    cityInput.setAttribute("id", "city");
     const labelCity = createTag("label", "for", "city");
-    cityInput.setAttribute("pattern", "/^[a-zA-Z]+(?:[\s-][a-zA-Z]+)*$");
+    cityInput.setAttribute("pattern", "^[a-zA-Z ,.'-]+$");
     cityInput.required = true;
     labelCity.textContent = "City:";
 
     const emailInput = createTag("input", "name", "email");
+    emailInput.setAttribute("id", "email");
     const labelEmail = createTag("label", "for", "email");
     emailInput.setAttribute("pattern", "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$");
     emailInput.required = true;
@@ -95,20 +113,14 @@ function buildForm() {
     formTag.appendChild(sendFormButton());
 }
 
-// ENVOYER ID & LENSE pour trouver l'index puis splice()
 function deleteItem(index) {
     let basket = [];
     basket = JSON.parse(localStorage.getItem("cart"));
-    console.log(basket);
-    console.log(typeof(basket));
-    console.log(index);
     basket.splice(index, 1);
     localStorage.setItem("cart", JSON.stringify(basket));
-    console.log(basket);
 }
 
 function buildBasket(localStorage) {
-    console.log(localStorage);
     let index = 0;
     for (const detail in localStorage) {
         const card = document.getElementById("basketCard");
